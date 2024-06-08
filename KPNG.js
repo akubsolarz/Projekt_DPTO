7e328595e33cec51415ad2b715455a80287d0014
akubsolarz <234617@student.uek.krakow.pl>
2024-06-09 01:04:14 +0200

    CSS update

--- KPNG.js
+++ KPNG.js
@@ -10,48 +10,50 @@ function start(Plewyb){
     const wyb = ['rock', 'paper', 'scissors'];
     const IAwyb = wyb[Math.floor(Math.random() *3)];
 
-    if(Plewyb === IAwyb){
-        tie++;
-    }
-    else if(
-        (Plewyb === 'rock' && IAwyb === 'scissors') ||
-        (Plewyb === 'scissors' && IAwyb === 'paper') ||
-        (Plewyb === 'paper' && IAwyb === 'rock'))   
-    {
-        win++;
-    }else{
-        los++;
+    if(game < maxs){
+        if(Plewyb === IAwyb){
+            tie++;
+        }
+        else if(
+            (Plewyb === 'rock' && IAwyb === 'scissors') ||
+            (Plewyb === 'scissors' && IAwyb === 'paper') ||
+            (Plewyb === 'paper' && IAwyb === 'rock'))   
+        {
+            win++;
+        }else{
+            los++;
+        }
+        
+        if(Plewyb === 'rock'){
+        document.getElementById('Playerimg').src = 'img/rock.png';
+        }else if(Plewyb === 'scissors'){
+        document.getElementById('Playerimg').src = 'img/scissors.png';
+        }else{
+        document.getElementById('Playerimg').src = 'img/papper.png';
+        }
+    
+        if(IAwyb === 'rock'){
+        document.getElementById('PCimg').src = 'img/rock.png';
+        }else if(IAwyb === 'scissors'){
+        document.getElementById('PCimg').src = 'img/scissors.png';
+        }else{
+        document.getElementById('PCimg').src = 'img/papper.png';
+        }
     }
     
-    if(Plewyb === 'rock'){
-    document.getElementById('Playerimg').src = 'img/rock.png';
-    }else if(Plewyb === 'scissors'){
-    document.getElementById('Playerimg').src = 'img/scissors.png';
-    }else{
-    document.getElementById('Playerimg').src = 'img/papper.png';
-    }
-
-    if(IAwyb === 'rock'){
-    document.getElementById('PCimg').src = 'img/rock.png';
-    }else if(IAwyb === 'scissors'){
-    document.getElementById('PCimg').src = 'img/scissors.png';
-    }else{
-    document.getElementById('PCimg').src = 'img/papper.png';
-    }
-
     game++;
 
     document.getElementById('won').textContent = win;
     document.getElementById('los').textContent = los;
     document.getElementById('tie').textContent = tie;
 
-    if(game === maxs){
+    if(game >= maxs){
         if(win > los){
             document.getElementById('zwy').textContent = 'wygrałeś';
-            return
+            return;
         }else if(los > win){
             document.getElementById('zwy').textContent = 'przegrałeś';
-        return;
+            return;
         }else{
             document.getElementById('zwy').textContent = 'remis';
             return;
